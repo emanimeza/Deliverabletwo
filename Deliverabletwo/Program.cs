01ee57fa18c32b9a68cad9ac6299a85c9f1d5d62
Emanuel <emanimeza@gmail.com>
2019-11-26 20:29:35 -0500

    First commit for deliverabletwo

---
+++ Deliverabletwo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Unit2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            string message = "";
+            int checksum = 0;
+
+            Console.Write(" What is your message? ");
+            input = Console.ReadLine();
+
+            input = input.ToUpper();
+
+            foreach (char c in input)
+            {
+
+                int code = (int)c;
+                checksum += code; //point 9 from rubric 
+                code = code - 64; //shifts the unicode value to code correctly
+                message += code.ToString() + "-"; //point 8 from rubric 
+
+            }
+            message = message.TrimEnd('-');
+
+            Console.WriteLine("Your encoded message is " + message); //point 10 from rubric 
+            Console.WriteLine("Message checksum is " + checksum);
+
+
+        }
+    }
+}
